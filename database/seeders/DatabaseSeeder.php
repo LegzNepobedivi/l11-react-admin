65a5d4a327350c73eb565a3be703d2eb6c635c72
Aleksandar <aleksa.jovanovic03@gmail.com>
2024-10-03 14:38:56 +0200

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Task;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Legzi2',
+            'email' => 'legzcina@gmail.com',
+            'password' => bcrypt('12345678'),
+            'email_verified_at' => now(),
+        ]);
+
+        Project::factory()
+        ->count(30)
+        ->hasTasks(30)
+        ->create();
+    }
+}
